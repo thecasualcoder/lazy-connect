92d29284e1926b8a169509334f0900bfee00909c
Arunvel Sriram <arunvelsriram@gmail.com>
2018-04-20 00:16:21 +0530

    Change project name, add function for connecting to VPN

---
+++ lazy-connect.sh
@@ -0,0 +1,90 @@
+#! /bin/bash
+
+function _lazy_connect_init() {
+  config_dir=~/.config/lazy-connect
+  echo -n "Secret Key: "
+  read -s secret_key
+  echo "**********"
+  mkdir -p $config_dir
+  echo $secret_key > $config_dir/secret
+
+  vpnNames=$(osascript <<EOF
+    tell application "System Events"
+      tell process "SystemUIServer"
+        set vpnMenu to (menu bar item 1 of menu bar 1 where description is "VPN")
+        tell vpnMenu to click
+        set vpnMenuItems to (menu items of menu 1 of vpnMenu)
+        -- Loop till first missing value(that is fisrt menu item seperator) and accumulate VPN Names
+        set vpnNames to {}
+        repeat with vpnMenuItem in vpnMenuItems
+          set vpnName to name of vpnMenuItem
+          if vpnName is equal to missing value then
+            exit repeat
+          end if
+          set vpnNames to vpnNames & {vpnName}
+        end repeat
+        key code 53
+        get vpnNames
+      end tell
+    end tell
+EOF
+)
+  echo $vpnNames | sed -e "s/Connect //g; s/Disconnect //g;" | tr , "\n" | xargs -I{} echo {} > $config_dir/vpns
+  echo "VPN List:"
+  cat $config_dir/vpns | nl
+}
+
+function _lazy_connect() {
+  vpn_name=$1
+  secret_key=$2
+  password=$(oathtool --totp --base32 $secret_key)
+
+  osascript <<EOF
+    on connectVpn(vpnName, password)
+      tell application "System Events"
+        tell process "SystemUIServer"
+          set vpnMenu to (menu bar item 1 of menu bar 1 where description is "VPN")
+          tell vpnMenu to click
+          try
+            click menu item ("Connect " & vpnName) of menu 1 of vpnMenu
+            delay 1
+            keystroke password
+            keystroke return
+          on error errorStr
+            if errorStr does not contain "Can’t get menu item" and errorStr does not contain vpnName then
+              display dialog errorStr
+            end if
+          end try
+        end tell
+      end tell
+    end connectVpn
+
+    connectVpn("$vpn_name", "$password")
+EOF
+}
+
+function lazy-connect() {
+  while getopts "iah" opt; do
+    case $opt in
+      h)
+        echo "Usage"
+        ;;
+      i)
+        _lazy_connect_init
+        ;;
+      a)
+        echo "Connect all"
+        ;;
+      \?)
+        echo "Invalid Option: -$OPTARG."
+        echo "Usage"
+        return 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument."
+        echo "Usage"
+        return 1
+        ;;
+    esac
+  done
+}
